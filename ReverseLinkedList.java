b3255bc2f58f7821870dc6ff9793f4ec746fb585
Anushka Maitra <anushka.mukti3011@gmail.com>
2025-08-30 02:12:23 +0530

    Create ReverseLinkedList.java

---
+++ ReverseLinkedList.java
@@ -0,0 +1,51 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class ReverseLinkedList {
+    public static Node reverseList(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next; // Store the next node
+            current.next = prev; // Reverse the current node's pointer
+            prev = current;      // Move prev and current one step forward
+            current = next;
+        }
+
+        head = prev; // Update the head to the new front
+        return head;
+    }
+
+    public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+
+        System.out.println("Original Linked List:");
+        printList(head);
+
+        head = reverseList(head);
+
+        System.out.println("Reversed Linked List:");
+        printList(head);
+    }
+}
